7599701e24f0236461fc8b3855806f82bc1ed0b8
Graeme Maclean <graemebmaclean@gmail.com>
2024-10-29 17:10:44 -0700

    measuring the three c's dcache misses

--- src/libs/cache_lib.c
+++ src/libs/cache_lib.c
@@ -39,6 +39,7 @@
 #include "debug/debug.param.h"
 #include "general.param.h"
 #include "libs/cache_lib.h"
+#include "libs/hash_lib.h"//mqi6
 #include "memory/memory.param.h"
 
 // DeleteMe
@@ -103,6 +104,9 @@ void init_cache(Cache* cache, const char* name, uns cache_size, uns assoc,
     init_cache_strategy(cache, name, cache_size, assoc, line_size, data_size, repl_policy);
     return;
   }
+    /* chages made by Ming and Graeme */
+  init_hash_table(&cache->access_history, "Cache Access History", 1024, sizeof(uns64));//mqi6
+  //cache->total_valid_cache_lines = 0;//mqi6
 
   /* set the basic parameters */
   strncpy(cache->name, name, MAX_STR_LENGTH);
@@ -271,7 +275,7 @@ void* cache_access(Cache* cache, Addr addr, Addr* line_addr, Flag update_repl) {
   return NULL;
 }
 
- 
+
 /**************************************************************************************/
 /* cache_insert: returns a pointer to the data section of the new cache line.
    Sets line_addr to the address of the first block of the new line.  Sets
@@ -770,6 +774,21 @@ uns cache_get_invalid_line_count(Cache* cache, Addr addr) {
     }
   }
   return invalid_entries;
+}
+
+    /* chages made by Ming and Graeme */
+Flag cache_is_full(Cache* cache) {
+    for (uns set = 0; set < cache->num_sets; set++) {
+        for (uns way = 0; way < cache->assoc; way++) {
+            Cache_Entry* entry = &cache->entries[set][way];
+            if (!entry->valid) {
+                // Found an invalid entry; cache is not full
+                return FALSE;
+            }
+        }
+    }
+    // All entries are valid; cache is full
+    return TRUE;
 }
 
 
